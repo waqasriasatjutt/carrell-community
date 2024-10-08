ffedb652e2d2d6a9f1479139efb125570ebba782
Mubasher <waqar.uaf.8936@gmail.com>
2024-10-09 00:04:02 +0500

    site_state

--- gt_rental_management/models/res_partner.py
+++ gt_rental_management/models/res_partner.py
@@ -1,20 +1,10 @@
 from datetime import datetime, date, time, timedelta
-import datetime
-from datetime import date
-from functools import partial
-from itertools import groupby
-from odoo.fields import Command
-
+from odoo.osv import expression
+from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
+from odoo.tools import float_is_zero, float_compare, float_round
 from odoo.tools.misc import formatLang
-from odoo.osv import expression
-from odoo.tools import float_is_zero, float_compare
-
-from odoo.addons import decimal_precision as dp
-
-from werkzeug.urls import url_encode
-from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
 
 class PartnerTemplate(models.Model):
