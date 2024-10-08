ac2cba5a0356a2b04ce3f74f2536d3632467f55c
Mubasher <waqar.uaf.8936@gmail.com>
2024-10-09 00:24:08 +0500

    partner

--- gt_rental_management/models/res_partner.py
+++ gt_rental_management/models/res_partner.py
@@ -1,27 +1,14 @@
-from datetime import datetime, date, time, timedelta
-import datetime
-from datetime import date
-from functools import partial
-from itertools import groupby
-from odoo.fields import Command
-
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError, ValidationError
-from odoo.tools.misc import formatLang
-from odoo.osv import expression
-from odoo.tools import float_is_zero, float_compare
-
-
-from odoo.addons import decimal_precision as dp
-
-from werkzeug.urls import url_encode
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
+
 
 class PartnerTemplate(models.Model):
     _inherit = "res.partner"
 
     customer_id = fields.Char('Customer ID')
     site_email = fields.Char('Site Email')
+    contact1_name = fields.Char('Contact 1 Name')
+    contact1_phone = fields.Char('Contact 1 Phone')
+    contact1_email = fields.Char('Contact 1 Email')
     contact2_name = fields.Char('Contact 2 Name')
     contact2_phone = fields.Char('Contact 2 Phone')
     contact2_email = fields.Char('Contact 2 Email')
@@ -34,4 +21,46 @@ class PartnerTemplate(models.Model):
     plug = fields.Char('Plug')
     number_of_pics = fields.Integer("Pics")
 
+    # Store Uses
+    dry_cont = fields.Char('Dry cont Ct(N)')
+    dry_trl = fields.Char('Dry Trl Ct')
+    reefer_ct = fields.Char('Reefer Ct')
+    doors_dry = fields.Char('Doors Dry')
+    doors_ref = fields.Char('Doors Ref')
+    cord = fields.Char('Cord Lgth')
 
+    reefer_type = fields.Selection(
+        string="Reefer Type",
+        selection=[('dr', 'Dr'), ('er', 'Er'), ('rc', 'Rc')],
+        required=False,
+        default='dr'  # Default value added here
+    )
+
+    # Cost Info
+    miles = fields.Char('Miles ($)')
+    gallons = fields.Float('Gallons')
+    fuel = fields.Float('Fuel Cost')
+    driver = fields.Float('Driver Pay')
+    expense = fields.Float('Expenses')
+    net = fields.Float('Net')
+    fuel_cost = fields.Float('Fuel Cost pG ($)')
+
+    # Rental Charges
+    dry_con_rent = fields.Float('Dry Con Rental')
+    dry_trl_rent = fields.Float('Dry Trailer Rental')
+    reefer_rent = fields.Float('Reefer Rental')
+    del_fee = fields.Float('Del Fee')
+    pickup_fee = fields.Float('Pickup Fee')
+    fuel_amount = fields.Float('Fuel sur amount(number)')  # Fixed typo here
+    fuel_sur = fields.Selection(
+        string="Fuel Sur",
+        selection=[('yes', 'Y'), ('no', 'N')],
+        required=False,
+        default='no'  # Default added
+    )
+    fuel_sc = fields.Selection(
+        string="Fuel SC Charge Type",
+        selection=[('mil', 'Mileage'), ('pc', 'Percent Choice')],
+        required=False,
+        default='mil'  # Default added
+    )
