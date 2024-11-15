32eb86cd2e00d80fb85f0f9083132b7be9c4fcce
waqas riasat <waqasriasatjutt@gmail.com>
2024-11-15 08:17:17 +0500

    push changes

--- carrell_transportion/models/sale_order.py
+++ carrell_transportion/models/sale_order.py
@@ -28,15 +28,15 @@ class SaleOrderGF(models.Model):
     
 
 
-    miles = fields.Integer(string='Miles')
-    miles_group = fields.Integer(string='Miles Group')
     mile_rate = fields.Float("Mile Rate")
     tons = fields.Float("Tons")
+
+    currency_id = fields.Many2one('res.currency', string='Currency', default=lambda self: self.env.company.currency_id)
 
     pro_number = fields.Char(string='Pro Number A')
     wo_number = fields.Char(string='Wo Number')
     po_number = fields.Char(string='Po Number')
-    rig = fields.Char(string='RIG')
+    rig = fields.Selection(string="RIG", selection=[('rig_2', 'RIG 2'), ('dan', 'DAN D'), ], required=False, )
     lease = fields.Char(string='Lease')
     operator = fields.Char(string='Operator')
     pu_at =  fields.Char(string='PU AT')
@@ -45,7 +45,7 @@ class SaleOrderGF(models.Model):
     product = fields.Many2one(
         comodel_name='product.product',
         string="Product")
-    driver_pay = fields.Float(string='Driver Pay')
+
     pin = fields.Char(string='PIN')
 
     street = fields.Char(string="Street")
@@ -54,8 +54,7 @@ class SaleOrderGF(models.Model):
 
 
     mp_number = fields.Char(string='MP Web Order Number')
-    bar_price = fields.Float(string='Bar Price')
-    flat_price = fields.Float(sting='Flat Price')
+
     fuel_s_rate = fields.Float(string='Fuel Sur Rate')
     trucking_cost = fields.Float(string='Trucking Cost')
     fuel_s_cost = fields.Float(string='Fuel Sur Cost')
@@ -114,7 +113,7 @@ class SaleOrderGF(models.Model):
     ('no', 'No'),
     ], string='Fork Lift', default='yes')
 
-    order_status = fields.Selection([ ('transfer', 'Transfer'), ('billed', 'Billed'), ('paid', 'Paid')], required=True, default='transfer')
+    # order_status = fields.Selection([ ('transfer', 'Transfer'), ('billed', 'Billed'), ('paid', 'Paid')], required=True, default='transfer')
     trailer = fields.Integer(string='Trailer')
     truck = fields.Char('Truck')
     notes = fields.Char('Notes')
@@ -126,5 +125,64 @@ class SaleOrderGF(models.Model):
 
 
 
+
+    bill_type = fields.Selection(string="Bill Type", selection=[('flat', 'Flat'), ('miles', 'Miles'), ('lb', '100 LB'), ], required=False, )
+    fc_type = fields.Selection(string="FC Type SEL", selection=[('none', 'None'), ('miles', 'Miles'), ('percent', 'Percent'), ], required=False, )
+    product_type = fields.Selection(string="Product Type", selection=[('all', 'All'), ('bar', '3.9 BAR'), ('bar_2', '4.1 Bar'), ('sack','SACK'), ('rental', 'Rental Only'), ], required=False, )
+    bar_type = fields.Selection(string="BAR Type", selection=[('secl', 'SECL 3.9'), ('owner', 'OWNER 4.1'), ('own_1', 'OWNED 3.9'), ('sell_4', 'SELL 4.1'), ('sell', 'SELL'), ('all', 'ALL') ], required=False, )
+
+
+    warehouse = fields.Float(string='Warehouse')
+
+
+    bar_price = fields.Monetary(string='Bar Price', currency_field='currency_id')
+    flat_price = fields.Monetary(sting='Flat Price', currency_field='currency_id')
+    fork_lift_price = fields.Monetary(sting='Fork Lift Charge', currency_field='currency_id')
+    labour_charge = fields.Monetary(sting='Stacking/Labour Charge', currency_field='currency_id')
+    rental_tank = fields.Monetary(sting='Rentals Tank', currency_field='currency_id')
+    rental_stand = fields.Monetary(sting='Rentals Stand', currency_field='currency_id')
+    rental_mat = fields.Monetary(sting='Rentals Matt', currency_field='currency_id')
+    rental_comp = fields.Monetary(sting='Rentals Comp', currency_field='currency_id')
+    miles_group = fields.Monetary(sting='Miles Group', currency_field='currency_id', digits=(16, 2))
+
+    rev_fixed_cost = fields.Monetary(sting='Rev Fixed Cost', currency_field='currency_id')
+    cpg = fields.Monetary(sting='CPG', currency_field='currency_id')
+    fuel_cost = fields.Monetary(sting='Fuel Cost(GL Used CPG)', currency_field='currency_id')
+
+
+    miles = fields.Float(string='Miles')
+    mpg = fields.Float(sting='MPG', default=5)
+    gall_used = fields.Float(string='Gall Used', compute='_compute_gall_used', store=True)
+
+    @api.depends('miles', 'mpg')
+    def _compute_gall_used(self):
+        for record in self:
+            record.gall_used = record.miles / record.mpg if record.mpg else 0
+
+    driver_over = fields.Float(string='Driver Overhead Percentage')
+    driver_pay = fields.Monetary(sting='Driver Pay', currency_field='currency_id', digits=(16, 2))
+    driver_total = fields.Float(string='Driver Total', compute='_compute_driver_total',  store=True )
+
+    @api.depends('driver_over', 'driver_pay')
+    def _compute_driver_total(self):
+        for record in self:
+            record.driver_total = record.driver_over * record.driver_pay
+
+    gross_expence = fields.Float(string='TOTS Of Expense', compute='_compute_gross_expence', store=True)
+
+    @api.depends('driver_total', 'fuel_cost')
+    def _compute_gross_expence(self):
+        for record in self:
+            record.gross_expence = record.driver_total + record.fuel_cost
+
+    gross_total = fields.Monetary(sting='Gross Total', currency_field='currency_id', digits=(16, 2))
+    net_total = fields.Monetary(string='Net Total', currency_field='currency_id', compute='_compute_net_total',
+                                store=True,)
+
+    @api.depends('gross_total', 'gross_expence')
+    def _compute_net_total(self):
+        for record in self:
+            # Calculate net_total as gross_total minus gross_expence
+            record.net_total = record.gross_total - record.gross_expence
 
 
